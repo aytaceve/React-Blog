154ba3510c9184f162234458dd8783759eaa4ad4
Aytac Ali <aytacaliyeva133@gmail.com>
2023-01-26 02:11:38 +0400

    fetching

--- my-blog/src/BlogList.js
+++ my-blog/src/BlogList.js
@@ -1,5 +1,5 @@
 
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
 
     return ( 
         <div className="blog-list">
@@ -8,7 +8,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
                 <div className="blog-preview" key={ blog.id }>
                     <h2>{ blog.title }</h2>
                     <p>Written by: { blog.author } </p>
-                    <button onClick={() => handleDelete( blog.id )}>Delete Blog</button>
                 </div>
            ))}
         </div>
--- my-blog/src/Home.js
+++ my-blog/src/Home.js
@@ -1,22 +1,14 @@
-import { useState } from "react";
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: "My New Website", body: 'lorem ipsum...', author: 'Mario', id: 1 },
-        { title: "Weolcome Party!", body: 'lorem ipsum...', author: 'Sailor', id: 2 },
-        { title: "Web dev top tips", body: 'lorem ipsum...', author: 'Mario', id: 3 }
-    ]);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
     return ( 
         <div className="home">
-           <BlogList blogs={ blogs } title="All Blogs" handleDelete={ handleDelete }/>
-           <BlogList blogs={ blogs.filter((blog) => blog.author === 'Mario') } title="Mario's Blogs" />
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div> }
+            {blogs && <BlogList blogs={ blogs } title="All Blogs" />}
         </div>
      );
 }
